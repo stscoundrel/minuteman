0d98e25118edb3ece208cec1aa6ceae7a46b82ca
Sampo Silvennoinen <20028934+stscoundrel@users.noreply.github.com>
2022-09-11 14:46:40 +0300

    Add minuteman core & time mappings

---
+++ src/minuteman/units.py
@@ -0,0 +1,90 @@
+from enum import Enum
+from typing import Final
+
+from pydantic import BaseModel
+
+
+class UnitOfTime(str, Enum):
+    SECONDS = "seconds"
+    MINUTES = "minutes"
+    HOURS = "hours"
+    DAYS = "days"
+    WEEKS = "weeks"
+    MONTHS = "months"
+    YEARS = "years"
+    DECADES = "decades"
+
+
+TIME_EXPRESSION_MULTIPLIERS: Final[dict[tuple[UnitOfTime, UnitOfTime], float]] = {
+    (UnitOfTime.SECONDS, UnitOfTime.SECONDS): 1,
+    (UnitOfTime.SECONDS, UnitOfTime.MINUTES): 1 / 60,
+    (UnitOfTime.SECONDS, UnitOfTime.HOURS): 1 / 60 / 60,
+    (UnitOfTime.SECONDS, UnitOfTime.DAYS): 1 / 60 / 60 / 24,
+    (UnitOfTime.SECONDS, UnitOfTime.WEEKS): 1 / 60 / 60 / 24 / 7,
+    (UnitOfTime.SECONDS, UnitOfTime.MONTHS): 1 / 60 / 60 / 24 / 30,
+    (UnitOfTime.SECONDS, UnitOfTime.YEARS): 1 / 60 / 60 / 24 / 365,
+    (UnitOfTime.SECONDS, UnitOfTime.DECADES): 1 / 60 / 60 / 24 / 365 / 10,
+    (UnitOfTime.MINUTES, UnitOfTime.SECONDS): 60,
+    (UnitOfTime.MINUTES, UnitOfTime.MINUTES): 1,
+    (UnitOfTime.MINUTES, UnitOfTime.HOURS): 1 / 60,
+    (UnitOfTime.MINUTES, UnitOfTime.DAYS): 1 / 60 / 24,
+    (UnitOfTime.MINUTES, UnitOfTime.WEEKS): 1 / 60 / 24 / 7,
+    (UnitOfTime.MINUTES, UnitOfTime.MONTHS): 1 / 60 / 24 / 30,
+    (UnitOfTime.MINUTES, UnitOfTime.YEARS): 1 / 60 / 24 / 365,
+    (UnitOfTime.MINUTES, UnitOfTime.DECADES): 1 / 60 / 24 / 365 / 10,
+    (UnitOfTime.HOURS, UnitOfTime.SECONDS): 60 * 60,
+    (UnitOfTime.HOURS, UnitOfTime.MINUTES): 60,
+    (UnitOfTime.HOURS, UnitOfTime.HOURS): 1,
+    (UnitOfTime.HOURS, UnitOfTime.DAYS): 1 / 24,
+    (UnitOfTime.HOURS, UnitOfTime.WEEKS): 1 / 24 / 7,
+    (UnitOfTime.HOURS, UnitOfTime.MONTHS): 1 / 24 / 30,
+    (UnitOfTime.HOURS, UnitOfTime.YEARS): 1 / 24 / 30 / 12,
+    (UnitOfTime.HOURS, UnitOfTime.DECADES): 1 / 24 / 30 / 12 / 10,
+    (UnitOfTime.DAYS, UnitOfTime.SECONDS): 24 * 60 * 60,
+    (UnitOfTime.DAYS, UnitOfTime.MINUTES): 24 * 60,
+    (UnitOfTime.DAYS, UnitOfTime.HOURS): 24,
+    (UnitOfTime.DAYS, UnitOfTime.DAYS): 1,
+    (UnitOfTime.DAYS, UnitOfTime.WEEKS): 1 / 7,
+    (UnitOfTime.DAYS, UnitOfTime.MONTHS): 1 / 30,
+    (UnitOfTime.DAYS, UnitOfTime.YEARS): 1 / 365,
+    (UnitOfTime.DAYS, UnitOfTime.DECADES): 1 / 365 / 10,
+    (UnitOfTime.WEEKS, UnitOfTime.SECONDS): 1 / 7 / 24 / 60 / 60,
+    (UnitOfTime.WEEKS, UnitOfTime.MINUTES): 1 / 7 / 24 / 60,
+    (UnitOfTime.WEEKS, UnitOfTime.HOURS): 1 / 7 / 24,
+    (UnitOfTime.WEEKS, UnitOfTime.DAYS): 1 / 7,
+    (UnitOfTime.WEEKS, UnitOfTime.WEEKS): 1,
+    (UnitOfTime.WEEKS, UnitOfTime.MONTHS): 1 / 4,
+    (UnitOfTime.WEEKS, UnitOfTime.YEARS): 1 / 52,
+    (UnitOfTime.WEEKS, UnitOfTime.DECADES): 1 / 52 / 10,
+    (UnitOfTime.YEARS, UnitOfTime.SECONDS): 31536000,
+    (UnitOfTime.YEARS, UnitOfTime.MINUTES): 525600,
+    (UnitOfTime.YEARS, UnitOfTime.HOURS): 8760,
+    (UnitOfTime.YEARS, UnitOfTime.DAYS): 365,
+    (UnitOfTime.YEARS, UnitOfTime.WEEKS): 52,
+    (UnitOfTime.YEARS, UnitOfTime.MONTHS): 12,
+    (UnitOfTime.YEARS, UnitOfTime.YEARS): 1,
+    (UnitOfTime.YEARS, UnitOfTime.DECADES): 1 / 10,
+    (UnitOfTime.DECADES, UnitOfTime.SECONDS): 315360000,
+    (UnitOfTime.DECADES, UnitOfTime.MINUTES): 5256000,
+    (UnitOfTime.DECADES, UnitOfTime.HOURS): 87600,
+    (UnitOfTime.DECADES, UnitOfTime.DAYS): 3650,
+    (UnitOfTime.DECADES, UnitOfTime.WEEKS): 520,
+    (UnitOfTime.DECADES, UnitOfTime.MONTHS): 120,
+    (UnitOfTime.DECADES, UnitOfTime.YEARS): 10,
+    (UnitOfTime.DECADES, UnitOfTime.DECADES): 1,
+}
+
+
+class TimeExpression(BaseModel):
+    amount: float
+    unit: UnitOfTime
+
+    @staticmethod
+    def get_multiplier_for(old_unit: UnitOfTime, new_unit: UnitOfTime) -> float:
+        return TIME_EXPRESSION_MULTIPLIERS[(old_unit, new_unit)]
+
+    def to_unit(self, new_unit: UnitOfTime) -> "TimeExpression":
+        return TimeExpression(
+            amount=self.amount * self.get_multiplier_for(new_unit, self.unit),
+            unit=new_unit,
+        )
---
+++ tests/test_core.py
@@ -0,0 +1,93 @@
+from unittest import TestCase
+
+from src.minuteman.core import (
+    TimeExpression,
+    TimeExpressionRequst,
+    UnitOfTime,
+    transform,
+)
+
+
+class TestMinutemanCore(TestCase):
+    def test_parses_time_transforms(self):
+        test_cases = [
+            # 5 minutes a day in 1 year in days
+            (
+                (
+                    TimeExpressionRequst(
+                        original=TimeExpression(
+                            amount=5,
+                            unit=UnitOfTime.MINUTES,
+                        ),
+                        in_time=TimeExpression(amount=1, unit=UnitOfTime.DAYS),
+                        comparison=TimeExpression(amount=1, unit=UnitOfTime.YEARS),
+                    ),
+                    UnitOfTime.DAYS,
+                ),
+                TimeExpression(amount=1.2673611111111112, unit=UnitOfTime.DAYS),
+            ),
+            # 1 hour a week in a year
+            (
+                (
+                    TimeExpressionRequst(
+                        original=TimeExpression(
+                            amount=1,
+                            unit=UnitOfTime.HOURS,
+                        ),
+                        in_time=TimeExpression(amount=1, unit=UnitOfTime.WEEKS),
+                        comparison=TimeExpression(amount=1, unit=UnitOfTime.YEARS),
+                    ),
+                    UnitOfTime.DAYS,
+                ),
+                TimeExpression(amount=2.1666666666666665, unit=UnitOfTime.DAYS),
+            ),
+            # 2 hours a week in 2 years
+            (
+                (
+                    TimeExpressionRequst(
+                        original=TimeExpression(
+                            amount=2,
+                            unit=UnitOfTime.HOURS,
+                        ),
+                        in_time=TimeExpression(amount=1, unit=UnitOfTime.WEEKS),
+                        comparison=TimeExpression(amount=2, unit=UnitOfTime.YEARS),
+                    ),
+                    UnitOfTime.DAYS,
+                ),
+                TimeExpression(amount=8.666666666666666, unit=UnitOfTime.DAYS),
+            ),
+            # 8 hours a day in a year in months
+            (
+                (
+                    TimeExpressionRequst(
+                        original=TimeExpression(
+                            amount=8,
+                            unit=UnitOfTime.HOURS,
+                        ),
+                        in_time=TimeExpression(amount=1, unit=UnitOfTime.DAYS),
+                        comparison=TimeExpression(amount=1, unit=UnitOfTime.YEARS),
+                    ),
+                    UnitOfTime.MONTHS,
+                ),
+                TimeExpression(amount=4.055555555555555, unit=UnitOfTime.MONTHS),
+            ),
+            # 5 days a week in a decade in years
+            (
+                (
+                    TimeExpressionRequst(
+                        original=TimeExpression(
+                            amount=5,
+                            unit=UnitOfTime.DAYS,
+                        ),
+                        in_time=TimeExpression(amount=1, unit=UnitOfTime.WEEKS),
+                        comparison=TimeExpression(amount=1, unit=UnitOfTime.DECADES),
+                    ),
+                    UnitOfTime.YEARS,
+                ),
+                TimeExpression(amount=7.123287671232877, unit=UnitOfTime.YEARS),
+            ),
+        ]
+
+        for args, expected in test_cases:
+            with self.subTest(args=args, expected=expected):
+                self.assertEqual(transform(*args), expected)
